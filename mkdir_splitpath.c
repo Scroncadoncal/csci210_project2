a10173af288e67766fdf5126878bfe4026779471
Mark Duffalo <mduffalo@mines.edu>
2024-10-27 17:44:41 -0600

    Hopefully fixed error on test 2, probably not on test 6, but we will hope that's fixed too

--- mkdir_splitpath.c
+++ mkdir_splitpath.c
@@ -29,7 +29,6 @@ void mkdir(char pathName[]) {
 
 	struct NODE* parentNode = splitPath(pathName, baseName, dirName);
 	if (parentNode == NULL) {
-		printf("MKDIR ERROR: no path provided\n");
 		return;
 	}
 	
@@ -90,7 +89,6 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName) {
 	if (dirEnd == pathName) {
 		strcpy(baseName, dirEnd + 1);
 		dirName[0] = '/';
-		return NULL;
 	} else {
 		int offset = dirEnd - pathName;
 		strncpy(dirName, pathName, offset);
